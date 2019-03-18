e3b903bf89c30173aceea1b282b47b279ebb3a98
Christian Engen <chris.a.engen@gmail.com>
2019-03-18 09:53:26 +0100

    Updates

---
+++ precache-manifest.ccc0a450b1add85f26514afdc4359310.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "150f2fc2ae1a52a432a5",
+    "url": "/procedural-dungeon/static/js/main.2245693e.chunk.js"
+  },
+  {
+    "revision": "04b72d50867a415a2bfe",
+    "url": "/procedural-dungeon/static/js/runtime~main.5f40d593.js"
+  },
+  {
+    "revision": "32889d4a602b88b60998",
+    "url": "/procedural-dungeon/static/js/2.ad8d4a5c.chunk.js"
+  },
+  {
+    "revision": "f280d20e44dba71e79df827edc42fd46",
+    "url": "/procedural-dungeon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fdbc6c9c5fa4399362426294e3c049e.js"
+  "/procedural-dungeon/precache-manifest.ccc0a450b1add85f26514afdc4359310.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/procedural-dungeon/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
