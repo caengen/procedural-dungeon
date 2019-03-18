e5f0dc4cc14aa25ff01e10a054085cfa93425608
Christian Engen <chris.a.engen@gmail.com>
2019-03-18 09:46:29 +0100

    Updates

---
+++ precache-manifest.7fdbc6c9c5fa4399362426294e3c049e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ea696d73531f058726b",
+    "url": "/static/js/main.07b04c64.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9263272d720f934c2bb7",
+    "url": "/static/js/2.44055920.chunk.js"
+  },
+  {
+    "revision": "7b0a90d0eecd181ba6fc47a9825532bf",
+    "url": "/index.html"
+  }
+];
